a25d6dd3a7b28e8afe1f3314e42f804588ccafe9
murc0 <92402819+murc0@users.noreply.github.com>
2024-03-26 13:07:17 -0400

    small script update
    added proper eggcompletion count
    added documentation
    removed herobrine

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 var root = document.querySelector(':root'); //declare root from styles
 var rootStyles = getComputedStyle(root); //get styles from root
 
+//get values of style variables
 var backColor = rootStyles.getPropertyValue('--backColor');
 var colorText = rootStyles.getPropertyValue('--colorText');
 
-let colors = ['red', 'blue', 'green']; //randomizing site starting color between r,g,b
+//randomizing site starting color between r,g,b
+let colors = ['red', 'blue', 'green']; 
 let numColors = colors.length;
 
+var eggCompletion = 0; //easter egg (eggCompletion/3) text
+
+//function to choose one of the three initial colors
 function getColor() {
     var num = Math.floor(Math.random() * numColors);
     
@@ -24,6 +29,7 @@ console.log('Background Color: ', chosenColor);
 var newColor = '';
 const acceptableColors = ['aliceblue','antiquewhite','aqua','aquamarine','azure','beige','bisque','black','blanchedalmond','blue','blueviolet','brown','burlywood','cadetblue','chartreuse','chocolate','coral','cornflowerblue','cornsilk','crimson','cyan','darkblue','darkcyan','darkgoldenrod','darkgray','darkgreen','darkkhaki','darkmagenta','darkolivegreen','darkorange','darkorchid','darkred','darksalmon','darkseagreen','darkslateblue','darkslategray','darkturquoise','darkviolet','deeppink','deepskyblue','dimgray','dodgerblue','firebrick','floralwhite','forestgreen','fuchsia','gainsboro','ghostwhite','gold','goldenrod','gray','green','greenyellow','honeydew','hotpink','indianred','indigo','ivory','khaki','lavender','lavenderblush','lawngreen','lemonchiffon','lightblue','lightcoral','lightcyan','lightgoldenrodyellow','lightgreen','lightgrey','lightpink','lightsalmon','lightseagreen','lightskyblue','lightslategray','lightsteelblue','lightyellow','lime','limegreen','linen','magenta','maroon','mediumaquamarine','mediumblue','mediumorchid','mediumpurple','mediumseagreen','mediumslateblue','mediumspringgreen','mediumturquoise','mediumvioletred','midnightblue','mintcream','mistyrose','moccasin','navajowhite','navy','navyblue','oldlace','olive','olivedrab','orange','orangered','orchid','palegoldenrod','palegreen','paleturquoise','palevioletred','papayawhip','peachpuff','peru','pink','plum','powderblue','purple','red','rosybrown','royalblue','saddlebrown','salmon','sandybrown','seagreen','seashell','sienna','silver','skyblue','slateblue','slategray','snow','springgreen','steelblue','tan','teal','thistle','tomato','turquoise','violet','wheat','white','whitesmoke','yellow','yellowgreen'];
 
+//function to create a new element of an image
 function showImage(src, width, height, alt) {
     var img = document.createElement('img');
     img.src = src;
@@ -47,18 +53,24 @@ function colorChange(newColor) { //repeating above steps every time user wants
         }
     }
     else if (newColor.toLowerCase() == 'marco') { //Easter Eggs:)
-        console.log('YOU FOUND AN EASTER EGG! (1/3)');
-        header.innerHTML = 'YOU FOUND AN EASTER EGG! (1/3)';
+        if (eggCompletion < 3)
+            eggCompletion++;
+        console.log('YOU FOUND AN EASTER EGG! (' + eggCompletion + '/3)');
+        header.innerHTML = 'YOU FOUND AN EASTER EGG! (' + eggCompletion + '/3)';
         showImage('images/HARD.jpg', 500, 300, 'marqito goat');
     }
     else if (newColor.toLowerCase() == 'josh') {
-        console.log('YOU FOUND AN EASTER EGG! (2/3)');
-        header.innerHTML = 'YOU FOUND AN EASTER EGG (2/3)';
+        if (eggCompletion < 3)
+            eggCompletion++;
+        console.log('YOU FOUND AN EASTER EGG! (' + eggCompletion + '/3)');
+        header.innerHTML = 'YOU FOUND AN EASTER EGG! (' + eggCompletion + '/3)';
         showImage('images/FullSizeRender.jpg', 500, 300, 'joshie');
     }
     else if (newColor.toLowerCase() == 'san') {
-        console.log('YOU FOUND AN EASTER EGG! (3/3)');
-        header.innerHTML = 'YOU FOUND AN EASTER EGG! (3/3)';
+        if (eggCompletion < 3)
+            eggCompletion++;
+        console.log('YOU FOUND AN EASTER EGG! (' + eggCompletion + '/3)');
+        header.innerHTML = 'YOU FOUND AN EASTER EGG! (' + eggCompletion + '/3)';
         showImage('images/eugene.jpg', 300, 500, 'san');
     }
     else {
